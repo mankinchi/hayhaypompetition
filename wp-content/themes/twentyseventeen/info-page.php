389c9f27eae8929d33c807fa4523b04db5fae038
Tri Nguyen <mankinchi.1995@gmail.com>
2017-04-13 18:57:01 +1000

    fix typos

--- wp-content/themes/twentyseventeen/info-page.php
+++ wp-content/themes/twentyseventeen/info-page.php
@@ -26,7 +26,7 @@
                     <div class="underline"></div>
                 </div>
                 <div class="content">
-                    <p><strong>BAY COMPETITION 2017</strong> được tồ chức bởi <strong>Elite TALKS Inc</strong>.  là cuộc thi nhằm tìm kiếm và phát triển tài năng về thuyết trình/hùng biện cho học sinh - sinh viên độ tuổi từ 14-23 tuổi, lần đầu tiên được tổ chức tại khu vực đồng bằng sông Cửu Long.</p>
+                    <p><strong>BAY COMPETITION 2017</strong> được tổ chức bởi <strong>Elite TALKS Inc</strong>.  là cuộc thi nhằm tìm kiếm và phát triển tài năng về thuyết trình/hùng biện cho học sinh - sinh viên độ tuổi từ 14-23 tuổi, lần đầu tiên được tổ chức tại khu vực đồng bằng sông Cửu Long.</p>
                     <p>Mùa đầu tiên của cuộc thi hứa hẹn phát hiện và ươm mầm cho những tài năng trẻ yêu thích thể hiện mình bằng khả năng hùng biện về những chủ đề đa dạng liên quan đến kiến thức văn hóa xã hội được cộng đồng quan tâm. Các tài năng trẻ còn được nhận các giải thưởng giá trị từ các nhà tài trợ lớn đến từ nhiều lãnh vực tại Úc.</p>
                     <p>Ban tổ chức tin rằng <strong>BAY COMPETITION</strong> sẽ là nơi truyền cảm hứng, chắp cánh cho sinh viên nói lên ước mơ và hoài bão của mình, giúp thế hệ trẻ Việt Nam phát triển tư duy, thái độ sống tích cực để các bạn có đủ hành trang bước ra hòa nhập với thế giới, trở thành công dân toàn cầu.</p>
                     <p>Đêm chung kết và trao giải sẽ diễn ra tại thành phố Cần Thơ.</p>
@@ -37,7 +37,7 @@
                                 <div class="">Thí sinh:</div>
                                 <ul>
                                     <li>
-                                        <div class="">Vòng Audition: 17/4- 22/5</div>
+                                        <div class="">Vòng Audition: 17/4- 30/5</div>
                                     </li>
                                     <li>
                                         <div class="">Vòng Bán Kết: 8/7 - 15/7 tại Cần Thơ</div>
@@ -67,7 +67,7 @@
                 </div>
                 <div class="content">
                     <p>Elite TALKS là tổ chức phi lợi nhuận và đăng kí hoạt động tại tiểu bang Victoria, Úc từ năm 2015. Elite TALKS tổ chức các hoạt động và diễn đàn thường niên cho cộng đồng trẻ nhằm mục đích truyền cảm hứng thông qua các câu chuyện thành công từ những nhân vật lãnh đạo, có sức ảnh hưởng với xã hội nói chung và cộng đồng sinh viên nói riêng.</p>
-                    <p>Diễn đàn Elite TALKS đa phần thu hút các diễn giả và đưa chủ đề hướng đến cộng đồng trẻ ở mọi ngành nghề và có độ tuổi dưới 35. Mục đích chung của ELITE TALKS bao gồm: định hướng giá trị cá nhân, chia sẻ kinh nghiệm thực tiễn, giúp mở rộng mối quan hệ, liên kết đến nghề nghiệp trong tương lai gần và truyền cảm hứng để tạo nên sự đột phá.</p>
+                    <p>Diễn đàn Elite TALKS đa phần thu hút các diễn giả và đưa chủ đề hướng đến cộng đồng trẻ ở mọi ngành nghề và có độ tuổi dưới 35. Mục đích chung của Elite TALKS bao gồm: định hướng giá trị cá nhân, chia sẻ kinh nghiệm thực tiễn, giúp mở rộng mối quan hệ, liên kết đến nghề nghiệp trong tương lai gần và truyền cảm hứng để tạo nên sự đột phá.</p>
                 </div>
             </div>
         </div>
@@ -92,7 +92,7 @@
                     <div class="underline"></div>
                 </div>
                 <div class="content">
-                    <p>Đội ngũ Ban Tổ Chức <strong>BAY COMPETITION</strong> là những thành viên đại diện Elite TALKS với tâm huyết tạo sân chơi và cơ hội thử thách đến các tài năng trẻ Việt Nam. Bên cạnh đó, không thể thiếu đội ngũ volunteer hùng hậu từ cộng đồng sinh viên các trường trung học, đại học lớn ở địa bàn thành phố Cần Thơ, Việt nam cũng như các du học sinh đang hoạt động tại các trường Đại học tại Melbourne, Úc.</p>
+                    <p>Đội ngũ Ban Tổ Chức <strong>BAY COMPETITION</strong> là những thành viên đại diện Elite TALKS với tâm huyết tạo sân chơi và cơ hội thử thách đến các tài năng trẻ Việt Nam. Bên cạnh đó, không thể thiếu đội ngũ volunteer hùng hậu từ cộng đồng sinh viên các trường trung học, đại học lớn ở địa bàn thành phố Cần Thơ, Việt Nam cũng như các du học sinh đang hoạt động tại các trường Đại học lớn tại Melbourne, Úc.</p>
                 </div>
             </div>
         </div>
@@ -131,7 +131,7 @@
                                         <li>Kỷ niệm chương và giấy chứng nhận từ Ban tổ chức.</li>
                                     </ul>
                                 </li>
-                                <li>Các giải học bổng tiền mặc khác cho các mạnh thường quân của chương trình tài trợ.</li>
+                                <li>Các giải học bổng tiền mặc khác do các mạnh thường quân của chương trình tài trợ.</li>
                             </ul>
                         </div>
                     </div>
@@ -145,8 +145,8 @@
                     <div class="underline"></div>
                 </div>
                 <div class="content">
-                    <p><strong>Vòng Audition:</strong> Mỗi thí sinh được yêu cầu nộp bài dự thi là một video (2-3 phút),thuyết trình về một chủ đề yêu thích của cá nhân. Video sẽ được đăng lên trang web cuộc thi và tiến hành bình chọn từ Ban Tổ chức và khán giả. Thí sinh có số phiếu kết bình chọn cao nhất từ khán giả qua hệ thống  website của chương trình sẽ được đặc cách vào thẳng vòng Gala Chung Kết.</p>
-                    <p><strong>Vòng Bán Kết:</strong> 15 thí sinh xuất sắc nhất chia đều trong 2 nhóm tuổi sẽ được chọn vào Bán kết. Các em sẽ được tham dự khoá huấn luyện từ các chuyên gia đến từ Ban giám khảo và có một tuần để chuẩn bị bài thuyết trình. Ban giám khảo sẽ chọn ra tổng cộng 7 thí sinh cuối cùng để vào vòng chung kết.</p>
+                    <p><strong>Vòng Audition:</strong> Mỗi thí sinh được yêu cầu nộp bài dự thi là một video (2-3 phút),thuyết trình về một chủ đề yêu thích của cá nhân. Video sẽ được đăng lên trang web cuộc thi và tiến hành bình chọn từ Ban Tổ chức và khán giả. Thí sinh có số phiếu kết quả bình chọn cao nhất từ khán giả qua hệ thống  website của chương trình sẽ được đặc cách vào thẳng vòng Gala Chung Kết.</p>
+                    <p><strong>Vòng Bán Kết:</strong> 15 thí sinh xuất sắc nhất chia đều trong 2 nhóm tuổi sẽ được chọn vào Bán kết. Các em sẽ được tham dự khoá huấn luyện từ các chuyên gia đến từ Ban giám khảo và có một tuần để chuẩn bị bài thuyết trình. Ban giám khảo sẽ chọn ra tổng cộng 7 thí sinh cuối cùng để vào vòng Gala Chung Kết.</p>
                     <p><strong>Vòng Gala Chung Kết:</strong> 8 thí sinh cuối cùng ( 7 thí sinh từ vòng Bán kết + 1 thí sinh được bình chọn vào thẳng)  sẽ thuyết trình trên sân khấu, so tài trong 2 vòng tại đêm Chung kết .Vòng đầu sẽ chọn ra 2 thí sinh xuất sắc nhất của mỗi nhóm tuổi. Vòng sau sẽ là cuộc so tài của 4 thí sinh để chọn ra  2 nhà vô địch cho mỗi nhóm tuổi.</p>
                 </div>
             </div>
--- wp-content/themes/twentyseventeen/template-parts/main-page/intro-modal.php
+++ wp-content/themes/twentyseventeen/template-parts/main-page/intro-modal.php
@@ -9,7 +9,7 @@
         <div class="row">
             <div class="col-md-6 col-md-offset-3">
                 <div class="second">
-                    <strong>BAY Competition 2017</strong> được tồ chức bởi <strong>Elite TALKS Inc</strong>. Là cuộc thi nhằm tìm kiếm và phát triển tài năng về thuyết trình/hùng biện cho học sinh - sinh viên độ tuổi từ 14-23 tuổi, lần đầu tiên được tổ chức tại khu vực đồng bằng sông Cửu Long. Mùa đầu tiên của cuộc thi hứa hẹn phát hiện và ươm mầm cho những tài năng trẻ yêu thích thể hiện mình bằng khả năng hùng biện về những chủ đề đa dạng liên quan đến kiến thức văn hóa xã hội được cộng đồng quan tâm. Các tài năng trẻ còn được nhận các giải thưởng giá trị từ các nhà tài trợ lớn đến từ nhiều lãnh vực tại Úc. <br />
+                    <strong>BAY Competition 2017</strong> được tổ chức bởi <strong>Elite TALKS Inc</strong>. Là cuộc thi nhằm tìm kiếm và phát triển tài năng về thuyết trình/hùng biện cho học sinh - sinh viên độ tuổi từ 14-23 tuổi, lần đầu tiên được tổ chức tại khu vực đồng bằng sông Cửu Long. Mùa đầu tiên của cuộc thi hứa hẹn phát hiện và ươm mầm cho những tài năng trẻ yêu thích thể hiện mình bằng khả năng hùng biện về những chủ đề đa dạng liên quan đến kiến thức văn hóa xã hội được cộng đồng quan tâm. Các tài năng trẻ còn được nhận các giải thưởng giá trị từ các nhà tài trợ lớn đến từ nhiều lãnh vực tại Úc. <br />
                 </div>
                 <div class="second hidden-xs hidden-sm">Ban tổ chức tin rằng <strong>BAY Competition</strong> sẽ là nơi truyền cảm hứng, chắp cánh cho sinh viên nói lên ước mơ và hoài bão của mình, giúp thế hệ trẻ Việt Nam phát triển tư duy, thái độ sống tích cực để các bạn có đủ hành trang bước ra hòa nhập với thế giới, trở thành công dân toàn cầu.</div>
             </div>
