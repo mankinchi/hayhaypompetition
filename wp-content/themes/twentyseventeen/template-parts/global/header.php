c2a8d55fd4b8a6d0b8878256c7b12b6d3733cf20
Tri Nguyen <mankinchi.1995@gmail.com>
2017-05-26 02:16:06 +1000

    Merge 2 header + 2 main page + Change top-header part

--- wp-content/themes/twentyseventeen/template-parts/global/header.php
+++ wp-content/themes/twentyseventeen/template-parts/global/header.php
@@ -11,12 +11,27 @@
 ?>
 <div class="row" position="0">
     <div class="col-xs-12 top-header">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/front-bg.jpg" class="bg">
         <div class="text row">
-            <p class="first">Are you ready to</p>
-            <p class="second">SPEAK YOUR FUTURE?</p>
-            <br />
-            <p class="third">Vòng Audition</p>
-            <p class="fourth">17/4 - 30/5</p>
+            <div class="col-md-3 col-md-offset-7 col-xs-11 col-xs-offset-1">
+                <p class="first">Cơ hội<br/>trở thành</p>
+                <p class="second">diễn giả trẻ</p>
+                <br />
+                <p class="third">Vòng Audition</p>
+                <div class="clock-wrapper">
+                    <div class="clock"></div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
+
+<script type="text/javascript">
+    var startDate = new Date();
+    var endDate = new Date('30 May 2017 23:59:59');
+    var diff = Math.floor((endDate - startDate) / 1000)
+    var clock = $(".clock").FlipClock(diff, {
+        clockFace: 'DailyCounter',
+        countdown: true,
+    });
+</script>
