52daa0d849a7c915ce427ea7ff07d0cf6374abe6
Tri Nguyen <mankinchi.1995@gmail.com>
2017-05-26 02:15:31 +1000

    Update intro modal
    - CSS part is included in next commit

--- wp-content/themes/twentyseventeen/template-parts/main-page/intro-modal.php
+++ wp-content/themes/twentyseventeen/template-parts/main-page/intro-modal.php
@@ -1,17 +1,68 @@
 <div class="row bottom-header page-modal" positionName="#gioi-thieu">
-    <div class="col-md-12 text">
+    <div class="col-md-8 col-md-offset-2 text">
         <div class="row">
-            <div class="col-md-6 col-md-offset-3">
+            <div class="col-md-12">
                 <p class="first">Giới thiệu chung</p>
                 <div class="underline"></div>
             </div>
         </div>
         <div class="row">
-            <div class="col-md-6 col-md-offset-3">
+            <div class="col-md-12">
                 <div class="second">
-                    <strong>BAY Competition 2017</strong> được tổ chức bởi <strong>Elite TALKS Inc</strong>. Là cuộc thi nhằm tìm kiếm và phát triển tài năng về thuyết trình/hùng biện bằng tiếng Việt cho học sinh - sinh viên độ tuổi từ 14-23 tuổi, lần đầu tiên được tổ chức tại khu vực đồng bằng sông Cửu Long. Mùa đầu tiên của cuộc thi hứa hẹn phát hiện và ươm mầm cho những tài năng trẻ yêu thích thể hiện mình bằng khả năng hùng biện tiếng Việt về những chủ đề đa dạng liên quan đến kiến thức văn hóa xã hội được cộng đồng quan tâm. Các tài năng trẻ còn được nhận các giải thưởng giá trị từ các nhà tài trợ lớn đến từ nhiều lãnh vực tại Úc. <br />
+                    <strong>BAY Competition 2017</strong> lần đầu tiên được tổ chức bởi <strong>Elite TALKS Inc</strong>. Đây là nơi truyền cảm hứng, chắp cánh cho sinh viên nói lên ước mơ và hoài bão của mình.
                 </div>
-                <div class="second hidden-xs hidden-sm">Ban tổ chức tin rằng <strong>BAY Competition</strong> sẽ là nơi truyền cảm hứng, chắp cánh cho sinh viên nói lên ước mơ và hoài bão của mình, giúp thế hệ trẻ Việt Nam phát triển tư duy, thái độ sống tích cực để các bạn có đủ hành trang bước ra hòa nhập với thế giới, trở thành công dân toàn cầu.</div>
+            </div>
+        </div>
+        <div class="row icon-row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="col-md-3 col-xs-6">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <img class="intro-image" src="<?php echo get_template_directory_uri(); ?>/assets/images/intro-icon-1.png" alt="image-sub"/>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="image-sub">Thuyết trình Tiếng Việt</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-xs-6">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <img class="intro-image" src="<?php echo get_template_directory_uri(); ?>/assets/images/intro-icon-2.png" alt="image-sub"/>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="image-sub">14-24t</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-xs-6">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <img class="intro-image" src="<?php echo get_template_directory_uri(); ?>/assets/images/intro-icon-3.png" alt="image-sub"/>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="image-sub">Đồng bằng sông Cửu Long</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-xs-6">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <img class="intro-image" src="<?php echo get_template_directory_uri(); ?>/assets/images/intro-icon-4.png" alt="image-sub"/>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="image-sub">Nhiều giải thưởng đến từ Úc</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <?php
