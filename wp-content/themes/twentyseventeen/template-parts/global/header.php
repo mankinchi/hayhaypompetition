cf76aa16536bb8bd2371e31047eb1d0d63257211
Tri Nguyen <mankinchi.1995@gmail.com>
2017-06-01 00:13:53 +1000

    Modify header

--- wp-content/themes/twentyseventeen/template-parts/global/header.php
+++ wp-content/themes/twentyseventeen/template-parts/global/header.php
@@ -16,22 +16,7 @@
             <div class="col-md-3 col-md-offset-7 col-xs-11 col-xs-offset-1">
                 <p class="first">Cơ hội<br/>trở thành</p>
                 <p class="second">diễn giả trẻ</p>
-                <br />
-                <p class="third">Vòng Audition</p>
-                <div class="clock-wrapper">
-                    <div class="clock"></div>
-                </div>
             </div>
         </div>
     </div>
 </div>
-
-<script type="text/javascript">
-    var startDate = new Date();
-    var endDate = new Date('30 May 2017 23:59:59');
-    var diff = Math.floor((endDate - startDate) / 1000)
-    var clock = $(".clock").FlipClock(diff, {
-        clockFace: 'DailyCounter',
-        countdown: true,
-    });
-</script>
